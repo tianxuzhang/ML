ebe8eff5c21b039db93bb7981d9944e244c998c1
tianxuzhang <987095891@qq.com>
2016-03-13 12:14:40 +0800

    linear algebra

---
+++ 1/linear_algebra.py
@@ -0,0 +1,31 @@
+﻿# -*- coding: utf-8 -*-
+from numpy import *
+# 方阵的行列式
+rand = random.rand(5,5)
+print '---Matrix Determinant---'
+print linalg.det(rand)
+
+# 逆
+print '---Matrix Inverse---'
+print rand
+print linalg.inv(rand)
+
+# 对称
+print '---symmetric matrix---'
+randT = rand.T
+print rand*rand.T
+
+# 秩
+print '---rank of matrix---'
+print linalg.matrix_rank(rand)
+
+# 可逆矩阵求解
+print '---solve invertible matrix---'
+a = [
+[1,-1,0],
+[-1,2,2],
+[1,1,-1]
+]
+b = [1,1,0]
+s = linalg.solve(a,b)
+print s
